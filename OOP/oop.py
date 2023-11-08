6d84bbf3fc8ac12b8e52f338b7cecd43e8d73d8e
KarenKuro <kkarenk888@gmail.com>
2023-11-08 10:44:32 +0400

    OOP

---
+++ OOP/oop.py
@@ -0,0 +1,38 @@
+# class Human:
+#     def __init__(self, n = '', a = 0, nt = '' ):
+#         self.name = n
+#         self.age = a
+#         self.nationality = nt
+#
+#
+# h1 = Human('James', 35, 'arm')
+# h1.
+# print(h1.name)
+
+# class Box:
+#     def __init__(self, a=0, b=0, c=0):
+#         self.l = a
+#         self.h = b
+#         self.v = c
+#
+#     def __str__(self):
+#         return  '%d, %d, %d'(self.l,self.h)
+#
+#
+# V = Box(5, 7, 8)
+# print(V)
+
+# class Square:
+#     def __init__(self, a=1):
+#         self.l = a
+#     def area(self):
+#         return self.l * self.l
+#     def perimetr(self):
+#         return self.l * 4
+#     def __str__(self):
+#         return '%d' (self.l)
+# sq = Square(8)
+# print(sq(area))
+# import string
+a = string.ascii_uppercase
+print(a)
